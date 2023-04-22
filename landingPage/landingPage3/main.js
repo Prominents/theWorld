ac70472d0151a4da7ca6c474889677f5ae17357c
Prominents <leonhardt722@gmail.com>
2023-04-22 10:41:55 +0700

    Landing Page

---
+++ landingPage/landingPage3/main.js
@@ -0,0 +1,42 @@
+TweenMax.staggerFrom(".logo", 1.6, {
+   delay: 0.2,
+   opacity: 0,
+   y: 20,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".search", 1.6, {
+   delay: 0.2,
+   opacity: 0,
+   y: 20,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".img", 2.5, {
+   delay: 0.7,
+   opacity: 0,
+   y: 70,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".bottom-bar", 2, {
+   delay: 1.5,
+   opacity: 0,
+   y: -40,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".side-icon", 2, {
+   delay: 2,
+   opacity: 0,
+   y: 40,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".title span", 3, {
+   delay: 3,
+   opacity: 0,
+   y: -100,
+   ease: Expo.easeInOut,
+ });
+ TweenMax.staggerFrom(".social ul li", 3, {
+   delay: 4,
+   opacity: 0,
+   y: 80,
+   ease: Expo.easeInOut,
+ });
